58b7c73949af2c9ef1604fa4398c0d01995a2a09
samuelleandro <113246079+samuelleandro@users.noreply.github.com>
2022-11-12 11:29:16 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var Ano_atual = '2022'
+        let Ano_de_nascimento = prompt('Digite Seu ano de nascimento')
+        let Nome = prompt('Digite seu nome')
+        let conta_para_sua_idade_atual
+        let idade = Ano_atual - Ano_de_nascimento
+        let completo = `Olá, seu nome é ${Nome} e você tem ${idade} anos`
+        alert (Ano_atual)
+        alert (idade)
+        alert (completo)
